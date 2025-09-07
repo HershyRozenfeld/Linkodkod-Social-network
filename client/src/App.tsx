ed04109e6178edce72db382cb59a1683857cafff
hershy rozenfeld <Y6764057@gmail.com>
2025-09-07 17:35:50 +0300

    secend part almost finished

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import AddPost from "./components/AddPost";
+import CreateFeed from "./components/CreateFeed";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <AddPost />
+    <CreateFeed />
     </>
   );
 }
