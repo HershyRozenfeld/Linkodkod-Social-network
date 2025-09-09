d276f8a5019ea1dc754a7262669ee22cee10b160
hershy rozenfeld <Y6764057@gmail.com>
2025-09-09 11:39:27 +0300

    added basic route and add a icon to navbar with route to home

--- client/src/application-layout/Logo.tsx
+++ client/src/application-layout/Logo.tsx
@@ -2,7 +2,8 @@ export default function Logo() {
   return (
     <>
       <div className="logo-area">
-        <img className="logo" src="./src/images/small-logo.jpeg" />
+        <img className="logo" src="http://localhost:3000/images/small-logo.jpeg" />
+        <img className="logo-text" src="http://localhost:3000/images/logo-text.jpeg" />
       </div>
     </>
   );
