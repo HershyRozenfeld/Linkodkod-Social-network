0fef44de0113e813d2ba0ee5a8401d0e8afdd6b7
Hershy Rozenfeld <153819469+HershyRozenfeld@users.noreply.github.com>
2025-09-17 10:23:05 +0300

    Update Logo.tsx

--- client/src/application-layout/Logo.tsx
+++ client/src/application-layout/Logo.tsx
@@ -2,9 +2,10 @@ export default function Logo() {
   return (
     <>
       <div className="logo-area">
-        <img className="logo" src="http://localhost:3000/images/small-logo.jpeg" />
-        <img className="logo-text" src="http://localhost:3000/images/logo-text.jpeg" />
+        <img className="logo" src="https://linkodkod-social-network.onrender.com/images/small-logo.jpeg" />
+        <img className="logo-text" src="https://linkodkod-social-network.onrender.com/images/logo-text.jpeg" />
       </div>
     </>
   );
 }
+
